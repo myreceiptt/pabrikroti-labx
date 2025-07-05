8eb5623c8b05930ece1b93d0d39d036ff9a731fe
Prof. NOTA <141787406+myreceiptt@users.noreply.github.com>
2025-07-05 10:25:49 +0700

    Create page.tsx

---
+++ src/app/bubu29/page.tsx
@@ -0,0 +1,16 @@
+// /src/app/bubu29/page.tsx
+
+"use client";
+
+// External libraries
+import React from "react";
+
+// Components libraries
+import FreeClaim from "@/components/contents/FreeClaim";
+import DynamicLoginPage from "@/components/logins/DynamicLogin";
+
+const PageBuBu29: React.FC = () => (
+  <DynamicLoginPage ContentComponent={() => <FreeClaim />} />
+);
+
+export default PageToken;
